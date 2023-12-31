2f3259e7bf18a3d83a1a92931bd6bae8e7035f15
Black Mathers <black.mathers08@gmail.com>
2023-12-31 14:18:21 +0300

    add all entities

---
+++ src/app.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CategoryModule } from './category/category.module';
+import { TransactionModule } from './transaction/transaction.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from "./user/user.module";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+@Module({
+  imports: [
+    UserModule,
+    CategoryModule,
+    TransactionModule,
+    AuthModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        synchronize: true,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      })
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+
+export class AppModule {
+}
